221f3b4e646a06469344e6393aaa294d59e691d6
mimseong <parkseongmin98@gmail.com>
2019-07-25 14:32:39 +0900

    연습용 A 이미지 넣고 테스트

--- readIMG.py
+++ readIMG.py
@@ -1,7 +1,6 @@
 #이미지파일 문자 읽어들이는 코드
 import cv2
 from matplotlib import pyplot as plt
-
 from pytesseract import image_to_string
 '''
 모듈 없을 시
@@ -10,7 +9,7 @@ pip install matplotlib
 '''
 
 #이미지 열기
-img = cv2.imread('CaptchaTest1.png')
+img = cv2.imread('D:\\Git\\BypassNaverCaptcha\\image\\A.jpg')
 
 #이미지 보기
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
