f71b545249bdfb48ded113f2a43cf79aaaabc973
mimseong <parkseongmin98@gmail.com>
2019-07-26 17:21:43 +0900

    테서렉트 코드(안 돌아감)
    에러도 안 뜨고 환경변수 설정도 다 했고 한데 작동하지 않음ㅠㅠㅠㅠㅠㅠ 테서렉트로 확인하고 머신러닝으로 넘어가려 했는데 바로 머신러닝 공부해야할듯

--- convertByTesseract.py
+++ convertByTesseract.py
@@ -1,6 +1,19 @@
+'''
+*****주의*****
+	동작하지 않음
+테서렉트로 하려 했는데 동작하지 않는 코드ㅜㅜ
+'''
+
 #C:\Program Files (x86)\Tesseract-OCR
+from pytesseract import *
+from PIL import Image
 
-from pytesseract import image_to_string
+def OCR(imgfile, lang ='eng') :
+    img = Image.open(imgfile)
+    text = image_to_string(img, lang = lang)
+    
+    print("OCR Result")
+    print(text)
 
-string_with_dates = image_to_string(img)
-print(string_with_dates)
+    
+OCR('D:\\Git\\BypassNaverCaptcha\\image\\A.jpg')
--- readIMG.py
+++ readIMG.py
@@ -1,9 +1,17 @@
+'''
+*****주의*****
+	동작하지 않음
+테서렉트로 하려 했는데 동작하지 않는 코드ㅜㅜ
+'''
+
+
 #이미지파일 문자 읽어들이는 코드
 import cv2
 from matplotlib import pyplot as plt
 from pytesseract import image_to_string
 '''
-모듈 없을 시
+모듈 없을 때 사용
+
 python -m pip install opencv-python
 pip install matplotlib
 '''
@@ -15,7 +23,6 @@ img = cv2.imread('D:\\Git\\BypassNaverCaptcha\\image\\A.jpg')
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.axis("off")
 plt.show()
-
 
 
 string_with_dates = image_to_string(img)
